d94fd5a5846eb366116c6fcc37b14acb825b5544
Noel Eck <noel.eck@intel.com>
2018-07-10 17:01:35 -0700

    test: Updated python unit test
    Hack to add working directory to sys.path in python when running
    via/ctest.

--- tests/python/test.py
+++ tests/python/test.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python
 
-import fir
-
 def main():
+    import fir
     filt = fir.Filter_LP(51, 1000, 10)
 
 if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        sys.path.insert(0, sys.argv[-1])
     main()
